8887ec193f44c46043e408b7ca061406528e988d
Ulvi Ahmadli <ulvi.ahmadli@msci.com>
2025-09-08 10:18:30 +0200

    Commit to Github

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    user_name = input("Enter your name: ")
+    user_name = input("Enter your name please: ")
     print(greet(user_name))
